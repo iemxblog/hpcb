e13088d147e64acfc0a7a5071edeb13cee3a1505
iemxblog <iemxblog@gmail.com>
2017-06-12 15:18:14 +0200

    Big  (and awful) commit after a long time of inactivity...

--- src/Kicad/Element.hs
+++ src/Kicad/Element.hs
@@ -8,16 +8,38 @@ module Kicad.Element
 
 import Kicad.SExpr
 
+newtype Layer = Layer String
+data Net = Net Int String
+
+data FpText = FpText
+  String      -- ^ Name of the text
+  String      -- ^ Content of the text
+  Position    -- ^ Position of the text
+  Layer       -- ^ Layer where the text is
+  Visibility  -- ^ Visibility of the text
+  Effects     -- ^ Effects (font, justification, etc.)
+
+data Visibility = Visible | Hidden
+data Effects = Effects Font [Justification]
+data Justification = Mirror | Center | Left | Right | Top | Bottom
+data Font = Font Size Thickness
+newtype Thickness = Thickness Float
+
 {-}
 data Module = Module String Layer String String Position ModuleDescription
 data ModuleDescription = ModuleDescription [Text] [Graphic] [Pad] Model
 -}
 
 data V2 a = V2 a a
-newtype Position = Position (V2 Float)
+
+-- | Position :
+-- V2 Float : Coordinates
+-- Maybe Float : Orientation
+data Position = Position (V2 Float) Float
 
 instance Itemizable Position where
-    itemize (Position (V2 x y))  = Item "position" [ParamFloat x, ParamFloat y]
+    itemize (Position (V2 x y) 0.0)  = Item "position" [ParamFloat x, ParamFloat y]
+    itemize (Position (V2 x y) o)  = Item "position" [ParamFloat x, ParamFloat y, ParamFloat o]
 
 newtype Size = Size (V2 Float)
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Kicad.SExpr
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  print $ Item "at" [ParamInt 2, ParamInt 3]
