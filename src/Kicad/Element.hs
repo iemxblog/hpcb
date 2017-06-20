2f2d646ed227d3da824b0c9b09591888c74f13e1
iemxblog <iemxblog@gmail.com>
2017-06-20 17:58:16 +0200

    Improved Elements.hs

--- src/Kicad/Element.hs
+++ src/Kicad/Element.hs
@@ -3,10 +3,19 @@ module Kicad.Element
     --Module(..)
     V2(..)
     , Layer(..)
+    , Net(..)
     , FpText(..)
     , Effects(..)
-    , At(..)
+    , Module(..)
+    , TEdit(..)
+    , TStamp(..)
+    , Position(..)
     , Size(..)
+    , Pad(..)
+    , PadType(..)
+    , PadShape(..)
+    , PadDrill(..)
+    , Graphic(..)
 ) where
 
 import Kicad.SExpr
@@ -18,11 +27,13 @@ instance Itemizable Layer where
   itemize (Layers xs) = Item "layers" $ map PString xs
 
 data Net = Net Int String
+instance Itemizable Net where
+  itemize (Net n s) = Item "net" [PInt n, PString s]
 
 data FpText = FpText
   String      -- ^ Name of the text
   String      -- ^ Content of the text
-  At          -- ^ Position of the text
+  Position    -- ^ Position of the text
   Layer       -- ^ Layer where the text is
   Effects     -- ^ Effects (font, justification, etc.)
 
@@ -36,6 +47,8 @@ instance Itemizable FpText where
         itemize effects
         ]
 
+-- | Effects are not implemented yet
+-- There is just a single constructor to provide one effect
 data Effects = StandardEffects
 
 instance Itemizable Effects where
@@ -47,19 +60,39 @@ instance Itemizable Effects where
         ]
       ]
 
-{-}
-data Module = Module String Layer String String Position ModuleDescription
-data ModuleDescription = ModuleDescription [Text] [Graphic] [Pad] Model
--}
+
+data Module = Module
+  String    -- ^ Module Name
+  Layer     -- ^ Module layer
+  TEdit     -- ^ Last edition time stamp
+  TStamp    -- ^ Time stamp from the schematic
+  Position  -- ^ Module position
+  [FpText]
+  [Graphic]
+  [Pad]
+
+
+instance Itemizable Module where
+  itemize (Module n l te ts pos texts graphics pads) =
+    Item "Module" ([PString n, itemize l, itemize te, itemize ts, itemize pos] ++ map itemize texts ++ map itemize graphics ++ map itemize pads)
+
+newtype TEdit = TEdit String
+instance Itemizable TEdit where
+  itemize (TEdit s)= Item "tedit" [PString s]
+
+newtype TStamp = TStamp String
+instance Itemizable TStamp where
+  itemize (TStamp s)= Item "tstamp" [PString s]
+
 
 data V2 a = V2 a a
 
 -- | Position :
 -- V2 Float : Coordinates
 -- Maybe Float : Orientation
-data At = At (V2 Float) (Maybe Float)
+data Position = At (V2 Float) (Maybe Float)
 
-instance Itemizable At where
+instance Itemizable Position where
     itemize (At (V2 x y) (Just 0.0))  = Item "at" [PFloat x, PFloat y]
     itemize (At (V2 x y) (Just o))  = Item "at" [PFloat x, PFloat y, PFloat o]
     itemize (At (V2 x y) Nothing)  = Item "at" [PFloat x, PFloat y]
@@ -68,3 +101,41 @@ newtype Size = Size (V2 Float)
 
 instance Itemizable Size where
     itemize (Size (V2 x y))  = Item "size" [PFloat x, PFloat y]
+
+data Pad = Pad
+  Int       -- ^ Pin number
+  PadType   -- ^ Through hole or SMD
+  PadShape
+  Position
+  Size
+  PadDrill
+  Layer
+  Net
+
+data PadType = ThroughHole | SMD
+instance Itemizable PadType where
+  itemize ThroughHole = PString "thru_hole"
+  itemize SMD = PString "smd"
+
+data PadShape = Circle | Rect | Oval
+instance Itemizable PadShape where
+  itemize Circle = PString "circle"
+  itemize Rect = PString "rect"
+  itemize Oval = PString "oval"
+
+newtype PadDrill = PadDrill Float
+instance Itemizable PadDrill where
+  itemize (PadDrill f) = Item "drill" [PFloat f]
+
+instance Itemizable Pad where
+  itemize (Pad number padType shape pos size drill layer net) =
+    Item "Pad" [PInt number, itemize padType, itemize shape, itemize pos, itemize size, itemize drill, itemize layer, itemize net]
+
+data Graphic = FpLine
+  (V2 Float)  -- ^ Line start
+  (V2 Float)  -- ^ Line end
+  Layer       -- ^ Layer
+  Float       -- ^ Line width
+
+instance Itemizable Graphic where
+  itemize (FpLine (V2 xs ys) (V2 xe ye) l w) = Item "fp_line" [Item "start" [PFloat xs, PFloat ys] , Item "end" [PFloat xe, PFloat ye], itemize l, PFloat w]
--- src/Main.hs
+++ src/Main.hs
@@ -7,3 +7,12 @@ main :: IO ()
 main = do
   print $ itemize (At (V2 3 4) Nothing)
   print $ itemize (FpText "reference" "R1" (At (V2 0 (-2.3)) (Just 45)) (Layer "F.Silks") StandardEffects)
+  print $ itemize $
+    Module "R3" (Layer "top_side.Cu") (TEdit "4E4C0E65") (TStamp "5127A136")
+      (At (V2 66.04 33.3502) Nothing)
+      [ FpText "reference" "R1" (At (V2 0 0.127) Nothing) (Layer "F.SilkS") StandardEffects,
+        FpText "value" "330k" (At (V2 0 0.127) Nothing) (Layer "F.SilkS") StandardEffects]
+      [ FpLine (V2 (-3.81) 0) (V2 (-3.302) 0) (Layer "F.SilkS") 0.2032]
+      [ Pad 1 ThroughHole Circle (At (V2 3.81 0) Nothing) (Size (V2 1.397 1.397)) (PadDrill 0.812799) (Layers ["*.Cu", "*.Mask", "F.SilkS"]) (Net 1 "/SIGNAL"),
+        Pad 2 ThroughHole Circle (At (V2 3.81 0) Nothing) (Size (V2 1.397 1.397)) (PadDrill 0.812799) (Layers ["*.Cu", "*.Mask", "F.SilkS"]) (Net 2 "GND")
+      ]
