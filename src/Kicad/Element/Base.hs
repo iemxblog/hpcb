dd73d28b881bd5c05bcbe11fe8bd058b98b1a569
iemxblog <iemxblog@gmail.com>
2017-07-02 12:53:16 +0200

    Added transformations (translation + rotation)

--- src/Kicad/Element/Base.hs
+++ src/Kicad/Element/Base.hs
@@ -1,6 +1,9 @@
 module Kicad.Element.Base(
   V2(..),
   Position(..),
+  Transformation,
+  translation,
+  rotation,
   Size(..),
   TEdit(..),
   TStamp(..)
@@ -14,6 +17,15 @@ data V2 a = V2 a a
 -- V2 Float : Coordinates
 -- Maybe Float : Orientation
 data Position = At (V2 Float) (Maybe Float)
+
+type Transformation = Position -> Position
+
+translation :: Float -> Float -> Transformation
+translation x y (At (V2 xp yp) a) = At (V2(xp+x) (yp+y)) a
+
+rotation :: Float -> Transformation
+rotation a (At (V2 x y) Nothing) = At (V2 x y) (Just a)
+rotation a (At (V2 x y) (Just ma)) = At (V2 x y) (Just (ma + a))
 
 instance Itemizable Position where
     itemize (At (V2 x y) (Just 0.0))  = Item "at" [PFloat x, PFloat y]
