b799625192b64c9f594c8dc82afa344fdbb31ede
iemxblog <iemxblog@gmail.com>
2017-07-04 22:03:24 +0200

    Added a Monoid instance for footprints

--- src/Kicad/Element/Footprint.hs
+++ src/Kicad/Element/Footprint.hs
@@ -7,8 +7,14 @@ import Kicad.Element.Base
 import Kicad.Element.Layer
 import Kicad.Element.FpElement
 
-data Footprint = Footprint String Layer TEdit TStamp Position [FpElement] -- ^ Name, layer, last edition time stamp, time stamp from the schematic, module position, ...
+data Footprint = Footprint String Layer TEdit TStamp Position FootprintContent -- ^ Name, layer, last edition time stamp, time stamp from the schematic, module position, ...
+
+newtype FootprintContent = FootprintContent [FpElement]
 
 instance Itemizable Footprint where
-  itemize (Footprint n l te ts pos fpElements) =
-    Item "Module" ([PString n, itemize l, itemize te, itemize ts, itemize pos] ++ map itemize fpElements)
+  itemize (Footprint n l te ts pos (FootprintContent fpContent)) =
+    Item "Module" ([PString n, itemize l, itemize te, itemize ts, itemize pos] ++ map itemize fpContent)
+
+instance Monoid FootprintContent where
+  mempty = FootprintContent []
+  FootprintContent xs `mappend` FootprintContent ys = FootprintContent (xs ++ ys)
