d84ba092287de54e7e9cee822b869d6b2a679248
iemxblog <iemxblog@gmail.com>
2017-07-05 15:35:12 +0200

    Corrected bug in prettyPrint

--- src/Hpcb/SExpr.hs
+++ src/Hpcb/SExpr.hs
@@ -43,7 +43,7 @@ prettyPrint' :: Bool -> Int -> Item -> String
 prettyPrint' b n (PString s) = indent b n s
 prettyPrint' b n (PFloat f) = indent b n $ showFFloat Nothing f ""
 prettyPrint' b n (PInt i) = indent b n $ show i
-prettyPrint' b n (Item s xs) | s `elem` noIndent = indent b n $ "(" ++ s ++ " " ++ unwords (map (prettyPrint' False n) xs) ++ ")"
+prettyPrint' b n (Item s xs) | s `elem` noIndent || not b = indent b n $ "(" ++ s ++ " " ++ unwords (map (prettyPrint' False n) xs) ++ ")"
   | otherwise = indent b n ("(" ++ s ++ " ") ++ concatMap (prettyPrint' b (n+1)) xs ++ indent b n ")"
 
 prettyPrint :: Item -> String
