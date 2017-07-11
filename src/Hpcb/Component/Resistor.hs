25c7866bec3f034f53d39502c36c15c10e8e87ce
iemxblog <iemxblog@gmail.com>
2017-07-11 22:41:02 +0200

    Added a 805 resistor footprint

--- src/Hpcb/Component/Resistor.hs
+++ src/Hpcb/Component/Resistor.hs
@@ -1,5 +1,6 @@
 module Hpcb.Component.Resistor (
-  r1206
+  r1206,
+  r805
 ) where
 
 import Hpcb.Data.Action
@@ -28,4 +29,22 @@ r1206 ref val = footprint "R_1206" $
   <> (
     pad 1 SMD Rect (V2 0.9 1.7) 0.1 (newNet ref 1) # translate (V2 (-1.45) 0)
     <> pad 2 SMD Rect (V2 0.9 1.7) 0.1 (newNet ref 2) # translate (V2 1.45 0)
+  ) # layers [FCu, FPaste, FMask]
+
+-- | SMD Resistor, 805 package (2012 metric)
+r805 :: String      -- ^ Reference
+        -> String   -- ^ Value
+        -> Footprint
+r805 ref val = footprint "R_805" $
+  fpText "reference" ref StandardEffects # translate (V2 0 (-2.3)) # layer FSilkS
+  <> fpText "value" val StandardEffects # translate (V2 0 2.3) # layer FFab
+  <> fpRectangle 2.0 1.25 0.1 # layer FFab
+  <> fpRectangle 3.2 2.0 0.05 # layer FCrtYd
+  <> (
+    fpLine (V2 0.6 0.0875) (V2 (-0.6) 0.875) 0.15
+    <> fpLine (V2 (-0.6) (-0.875)) (V2 0.6 (-0.875)) 0.15
+  ) # layer FSilkS
+  <> (
+    pad 1 SMD Rect (V2 0.7 1.3) 0.1 (newNet ref 1) # translate (V2 (-0.95) 0)
+    <> pad 2 SMD Rect (V2 0.7 1.3) 0.1 (newNet ref 2) # translate (V2 0.95 0)
   ) # layers [FCu, FPaste, FMask]
