4c2111115cef593f3baab7d801befc6f5d92e29b
iemxblog <iemxblog@gmail.com>
2017-07-13 13:25:01 +0200

    Updated a test

--- tests/Hpcb/NetNumberingSpec.hs
+++ tests/Hpcb/NetNumberingSpec.hs
@@ -10,16 +10,17 @@ import Hpcb.Data.Layer
 import Hpcb.Data.Effects
 import Hpcb.Data.Net
 import Hpcb.Data.Footprint
+import Hpcb.Data.Circuit
 
 spec :: SpecWith ()
 spec = describe "Net numbering" $ do
-          describe "listOfNetsFootprint" $ do
+          describe "listOfNets" $ do
             it "returns the correct list of nets for a basic list of FpElement" $
-              listOfNetsFootprint (Footprint "R1" FCu dummyTEdit dummyTStamp origin (FpContent [
+              listOfNets (Circuit [Footprint "R1" FCu dummyTEdit dummyTStamp origin (FpContent [
                 FpLine (V2 1 2) (V2 3 4) FCu 1,
                 FpCircle (V2 1 2) (V2 3 4) BCu 1,
                 FpText "ref" "r1" (At (V2 1 2) Nothing) FCu StandardEffects,
                 Pad 1 SMD Rect (At (V2 3 4) Nothing) (V2 1 2) 0.5 [FCu, FPaste, FMask] (Net "/SIGNAL"),
                 Pad 2 SMD Rect (At (V2 3 4) Nothing) (V2 1 2) 0.5 [FCu, FPaste, FMask] (Net "GND"),
                 Pad 3 SMD Rect (At (V2 3 4) Nothing) (V2 1 2) 0.5 [FCu, FPaste, FMask] (Net "VCC")
-              ])) `shouldBe` ["/SIGNAL", "GND", "VCC"]
+              ])] [] []) `shouldBe` [Net "/SIGNAL", Net"GND", Net "VCC"]
