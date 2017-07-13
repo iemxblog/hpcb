799c8baf2f475bd553baa8d508bacaf35fb06209
iemxblog <iemxblog@gmail.com>
2017-07-13 14:03:37 +0200

    Changed main function example

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,11 @@
 module Main where
 
 import Hpcb.SExpr
-import Hpcb.Data
-import Hpcb.Functions
 import Hpcb.NetNumbering
+import Hpcb.Component
+import Hpcb.Data.Circuit
 import Data.Monoid
 
 main :: IO ()
-main = do
-  --print $ itemize (At (V2 3 4) Nothing)
-  --print $ itemize (FpText "reference" "R1" (At (V2 0 (-2.3)) (Just 45)) FSilkS StandardEffects)
-  {-
-  putStr $ prettyPrint $ itemize $
-    Footprint "R3" FCu (TEdit "4E4C0E65") (TStamp "5127A136")
-      (At (V2 66.04 33.3502) Nothing)
-      [ FpText "reference" "R1" (At (V2 0 0.127) Nothing) FSilkS StandardEffects,
-        FpText "value" "330k" (At (V2 0 0.127) Nothing) FSilkS StandardEffects,
-        FpLine (V2 (-3.81) 0) (V2 (-3.302) 0) FSilkS 0.2032,
-        Pad 1 ThroughHole Circle (At (V2 3.81 0) Nothing) (Size (V2 1.397 1.397)) (PadDrill 0.812799) (copperLayers ++ maskLayers ++ [FSilkS]) (Net 1 "/SIGNAL"),
-        Pad 2 ThroughHole Circle (At (V2 3.81 0) Nothing) (Size (V2 1.397 1.397)) (PadDrill 0.812799) (copperLayers  ++ maskLayers ++ [FSilkS]) (Net 2 "GND")
-      ]
-      -}
-  putStr $ prettyPrint $ itemize $ numberNets $ Circuit [
-    translate (V2 66.04 33.3502) $
-      layer FCu $
-        footprint "R1" $
-          translate (V2 0 0.127) $
-            layer FSilkS (
-              fpText "reference" "R1" StandardEffects
-              <> fpText "value" "330k" StandardEffects
-              <> fpLine (V2 (-3.81) 0) (V2 (-3.302) 0) 0.2032
-            )
-            <> translate (V2 3.81 0) (pad 1 ThroughHole Circle (V2 1.397 1.397) 0.812799 (Net "/SIGNAL"))
-            <> translate (V2 3.81 0) (pad 1 ThroughHole Circle (V2 1.397 1.397) 0.812799 (Net "GND"))
-    ] [] []
+main =
+  putStr $ prettyPrint $ itemize $ numberNets $ Circuit [r805 "R1" "10k"] [] []
